39c918c2bb613f7b7e407197b24e4e09e55ac3b5
quangduccao <onggieoi@gmail.com>
2020-11-20 09:36:28 +0700

    refactor from admin next app

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -2,13 +2,7 @@ module.exports = {
   webpack: (config) => Object.assign(config, {
     target: 'electron-renderer',
   }),
-  env: {
-    // Getway in prod
-    URL_GETWAY: 'http://api.makemon.online/graphql',
-
-    URL_EXCEL: 'http://app-booking.dalattourist.com.vn/ops/statistic',
-
-    // Local Getway
-    // URL_GETWAY: 'http://192.168.3.118:3001/graphql',
-  },
+  // env: {
+    
+  // },
 };
